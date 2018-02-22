33c7f924a0d1d04a9720d0038bc7bc8015bb7333
Petit-Chaperon-Rouge <axel.lecoeuche@sfr.fr>
2018-02-22 11:42:32 +0100

    CMakeLists fait

---
+++ TP_compilation/build/CMakeFiles/tpx.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tpx.dir/sayHello/sayHello.cpp.o"
+  "CMakeFiles/tpx.dir/sayHello/module1.cpp.o"
+  "tpx.pdb"
+  "tpx"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tpx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
