f50dc5128098f4f73b3c7e06f4ca0f32966e47ec
Petit-Chaperon-Rouge <axel.lecoeuche@sfr.fr>
2018-04-19 09:29:08 +0200

    Milieu TP5

---
+++ TP_tests/fibo/build/CMakeFiles/main_test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_test.dir/src/main_test.cpp.o"
+  "CMakeFiles/main_test.dir/src/Fibo.cpp.o"
+  "CMakeFiles/main_test.dir/src/Fibo_test.cpp.o"
+  "main_test.pdb"
+  "main_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- TP_tests/fibo/src/print_fibo.cpp
+++ TP_tests/fibo/src/print_fibo.cpp
@@ -1,9 +1,26 @@
 #include "Fibo.hpp"
 #include <iostream>
+// uncomment to disable assert()
+// #define NDEBUG
+#include <cassert>
+#include <string>
 
 int main() {
-    for (int i=0; i<50; i++)
-        std::cout << fibo(i) << std::endl;
-    return 0;
+  try {
+    for (int i=0; i<50; i++) {
+      // assert(fibo(i) >= 0);
+      // assert(fibo(i) >= fibo(i-1));
+      if (fibo(i) < 0)
+	throw std::string("Error: fibo(i) < 0");
+      if (fibo(i) < fibo(i-1))
+	throw std::string("Error: fibo(i) < fibo(i-1)");
+      std::cout << fibo(i) << std::endl;
+    }
+  }
+  catch (std::string str)
+    {
+      std::cout << str << std::endl;
+    }
+  return 0;
 }
 
